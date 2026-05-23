52f04c3cbfd81048b168d2862c57765eacacf73d
unknown <pwtoni1sampai10@gmail.com>
2026-05-23 09:24:05 +0700

    membuat website kenangan mantan

---
+++ haha.js
@@ -0,0 +1,14 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klick diluar sidebar untuk menghilangkan nav//
+const menu = document.querySelector("#menu");
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
